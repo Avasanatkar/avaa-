63be1425645446b55a37045ebbf6dd3e3a56d2de
ava <sanatkarava@gmail.com>
2025-03-21 12:48:40 +0000

    mannn

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("heloommm");
+        System.out.println("heloommmmm");
         System.out.println("heloo");
     }
 }
