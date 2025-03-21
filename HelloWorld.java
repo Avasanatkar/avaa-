57808b63367c11fb54285c1f0e062ce6d2fd5b3b
ava <sanatkarava@gmail.com>
2025-03-21 12:45:58 +0000

    chjsb

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("heloommm");
+        System.out.println("heloo");
     }
 }
